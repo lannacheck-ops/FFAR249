42634d5101ea781b220b6cd013c56b336e4993c2
Lanna Check <lannacheck@gmail.com>
2026-04-09 23:21:25 -0400

    Added the coded to move the spotlight left to right

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,2 +1,28 @@
+let light = document.querySelector("#light");
+let spotLight = document.querySelector("#spotLight");
+
 window.onload = function () {
+    let lightX = parseInt(getComputedStyle(light).left);
+    let spotLightX = parseInt(getComputedStyle(spotLight).left);
+    window.addEventListener("keydown", moveLight);
+
+    function moveLight(e) {
+        e.preventDefault()
+        if (e.code === "ArrowLeft" && lightX > 520) {
+            lightX -= 5;
+            spotLightX = lightX - 130;
+            light.style.left = lightX + "px"
+            spotLight.style.left = spotLightX + "px"
+        }
+        if (e.code === "ArrowRight" && lightX < 1180) {
+            lightX += 5;
+            spotLightX = lightX - 130;
+            light.style.left = lightX + "px"
+            spotLight.style.left = spotLightX + "px"
+        }
+        console.log(lightX);
+    }
 };
+
+
+
